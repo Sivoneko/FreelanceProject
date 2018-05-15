c13ca02dfb9c982af77b8901e1fc971b79aad747
Sivoneko <34922742+Sivoneko@users.noreply.github.com>
2018-05-15 23:04:18 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,479 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+T* AddStruct(T* Obj, const int amount)
+{
+	if (amount == 0)
+	{
+		Obj = new T[amount + 1];
+	}
+	else
+	{
+		T* tempObj = new T[amount + 1];
+		for (int i(0); i < amount; i++)
+		{
+			tempObj[i] = Obj[i];
+		}
+		delete[] Obj;
+
+		Obj = tempObj;
+	}
+	return Obj;
+}
+
+
+struct Buyer
+{
+	string Name, SecondName, ThirdName, adress,phone, InditificationNumber;
+	int CarAmount;
+	Buyer() { Name.resize(0), SecondName.resize(0), ThirdName.resize(0), adress.resize(0), phone.resize(0),
+		InditificationNumber.resize(0), CarAmount = 0; }
+	/*
+	Buyer(const Buyer& obj)
+	{
+			Name = obj.Name;
+			SecondName = obj.Name;
+			ThirdName = obj.ThirdName;
+			adress = obj.adress;
+			phone = obj.phone;
+			InditificationNumber = obj.InditificationNumber;
+			CarAmount = obj.CarAmount;
+	}
+	*/
+	friend istream& operator>>(istream& is, Buyer& obj)
+	{
+		cout << "Input Name: ";
+		cin.get();
+		getline(cin, obj.Name);
+		cout << "Input Second Name: ";
+		//cin.get();
+		getline(cin, obj.SecondName);
+		cout << "Input Third Name: ";
+		//cin.get();
+		getline(cin, obj.ThirdName);
+		cout << "Input adress: ";
+		//cin.get();
+		getline(cin, obj.adress);
+		cout << "Input phone: ";
+		//cin.get();
+		getline(cin, obj.phone);
+		cout << "Input Indification number: ";
+		//cin.get();
+		getline(cin, obj.InditificationNumber);
+		return is;
+	}
+};
+struct Manager
+{
+	string Name, SecondName, ThirdName, adress, phone;
+	int year;
+	double seniority;
+	Buyer * buyer;
+	int AmountBuyer;
+
+	Manager()
+	{
+		Name.resize(0), SecondName.resize(0), ThirdName.resize(0), adress.resize(0), phone.resize(0);
+		year = 0;
+		seniority = 0;
+		buyer = 0;
+		AmountBuyer = 0;
+	}
+	Manager(const Manager& obj)
+	{
+		Name = obj.Name;
+		SecondName = obj.SecondName;
+		ThirdName = obj.ThirdName;
+		adress = obj.adress;
+		phone = obj.phone;
+		year = obj.year;
+		seniority = obj.seniority;
+		AmountBuyer = obj.AmountBuyer;
+
+		if (AmountBuyer != 0)
+		{
+			buyer = new Buyer[AmountBuyer];
+			for (int i(0); i < AmountBuyer; i++)
+			{
+				buyer[i] = obj.buyer[i];
+			}
+		}
+	}
+	Manager& operator=(const Manager& manager)
+	{
+		if (this != &manager && &manager != NULL)
+		{
+			Name.clear();
+			SecondName.clear();
+			ThirdName.clear();
+			adress.clear();
+			phone.clear();
+			delete[] buyer;
+
+			Name = manager.Name;
+			SecondName = manager.SecondName;
+			ThirdName = manager.ThirdName;
+			adress = manager.adress;
+			phone = manager.phone;
+			year = manager.year;
+			seniority = manager.seniority;
+
+			if (manager.AmountBuyer != 0)
+			{
+
+				AmountBuyer = manager.AmountBuyer;
+
+				buyer = new Buyer[manager.AmountBuyer];
+				for (int i(0); i < manager.AmountBuyer; i++)
+				{
+					buyer[i] = manager.buyer[i];
+				}
+			}
+
+		}
+		return *this;
+	}
+
+	void SetBuyer(const Buyer& B)
+	{
+		buyer = AddStruct(buyer, AmountBuyer);
+		buyer[AmountBuyer] = B;
+		AmountBuyer++;
+	}
+
+	friend istream& operator>>(istream& is, Manager& obj)
+	{
+		cout << "Input Name: ";
+		cin.get();
+		getline(cin, obj.Name);
+		cout << "Input Second Name: ";
+		cin.get();
+		getline(cin, obj.SecondName);
+		cout << "Input Third Name: ";
+		cin.get();
+		getline(cin, obj.ThirdName);
+		cout << "Input adress: ";
+		cin.get();
+		getline(cin, obj.adress);
+		cout << "Input phone: ";
+		cin.get();
+		getline(cin, obj.phone);
+		cout << "Input Year: ";
+		cin >> obj.year;
+		cout << "Input Seniority: ";
+		cin >> obj.seniority;
+		return is;
+	}
+	friend ostream& operator<<(ostream& os, Manager& obj)
+	{
+		cout << "Name: " << obj.Name << "\t Second Name: " << obj.SecondName << "\t Third Name: " << obj.ThirdName << "\t Adress: " << obj.adress
+			<< "\t Phone: " << obj.phone << "\t Birthday: " << obj.year << "\t Seniority: " << obj.seniority;
+		return os;
+	}
+};
+struct Car
+{
+	string Name,Model;
+	int year;
+	double EngineCapacity,BodyVolume,prize;
+	Manager* manager;
+	Buyer buyer;
+	int AmountManager;
+	Car()
+	{
+		//Name.resize(0), Model.resize(0);
+		year = 0;
+		EngineCapacity = 0, BodyVolume = 0, prize = 0;
+		manager = 0;
+		AmountManager = 0;
+	}
+	Car(const Car& car)
+	{
+		Name = car.Name;
+		Model = car.Model;
+		year = car.year;
+		EngineCapacity = car.EngineCapacity;
+		BodyVolume = car.BodyVolume;
+		prize = car.prize;
+		buyer = car.buyer;
+		AmountManager = car.AmountManager;
+		if (AmountManager != 0)
+		{
+			manager = new Manager[AmountManager];
+
+			for (int i(0); i < AmountManager; i++)
+			{
+				manager[i] = car.manager[i];
+			}
+		}
+	}
+	Car& operator=(const Car& car)
+	{
+		if (this != &car && &car != NULL)
+		{
+			Name.clear();
+			Model.clear();
+			Name = car.Name;
+			Model = car.Model;
+			year = car.year;
+			EngineCapacity = car.EngineCapacity;
+			BodyVolume = car.BodyVolume;
+			prize = car.prize;
+			buyer = car.buyer;
+			delete[] manager;
+			if (car.AmountManager != 0)
+			{
+				AmountManager = car.AmountManager;
+				manager = new Manager[car.AmountManager];
+
+				for (int i(0); i < car.AmountManager; i++)
+				{
+					manager[i] = car.manager[i];
+				}
+			}
+		}
+		return *this;
+	}
+	
+	//присвоение машине менеджера, их может быть несколько
+	void SetManager(const Manager& B)
+	{
+		manager = AddStruct(manager, AmountManager);
+		manager[AmountManager] = B;
+		AmountManager++;
+	}
+	//присвоение машине покупателя, может быть только один
+	void SetBuyer(const Buyer& B)
+	{
+		buyer = B;
+	}
+	void CarDelete()
+	{
+		Name.clear();
+		Model.clear();
+		year = 0;
+		EngineCapacity = 0;
+		BodyVolume = 0;
+		prize = 0;
+		delete[] manager;
+		AmountManager = 0;
+	}
+
+	friend istream& operator>>(istream& is, Car& obj)
+	{
+		cout << "Input Name: ";
+		cin.get();
+		getline(cin, obj.Name);
+		cout << "Input Model: ";
+		//cin.get();
+		getline(cin, obj.Model);
+		cout << "Input year: ";
+		cin >> obj.year;
+		cout << "Input Engine Capacity: ";
+		cin >> obj.EngineCapacity;
+		cout << "Input Body volume: ";
+		cin >> obj.BodyVolume;
+		cout << "Enter prize: ";
+		cin >> obj.prize;
+		/*
+		cout << "Add Manager: ";
+		obj.manager = AddStruct(obj.manager, obj.AmountManager);
+		cin >> obj.manager[obj.AmountManager];
+		obj.AmountManager++;
+		*/
+		return is;
+	}
+	friend ostream& operator<<(ostream& os, Car& obj)
+	{
+		cout <<"Name: "<< obj.Name <<" "<<"\t Model: "<<obj.Model<<"\t Year: "<<obj.year<< "\t Engine Capacity: "<< obj.EngineCapacity<<
+			"\t Body Volume: "<< obj.BodyVolume<< "\t Prize: "<<obj.prize;
+		return os;
+	}
+};
+void SetCar(Car* car, int amount)
+{
+	cin >> car[amount];
+}
+
+//поиск для общих случаев
+/*
+template<typename T>
+bool FindAllCases(const T a1, int Case)
+{
+	switch (Case)
+	{
+	case '0':
+	{
+		cout << "Enter name: ";
+		string Name;
+		cin.get();
+		getline(cin, Name);
+		return FindByName(a1, Name);
+	}
+	case '1':
+	{
+		cout << "Enter Second Name: ";
+		string SecondName;
+		cin.get();
+		getline(cin, SecondName);
+		return FindBySecondName(a1,SecondName);
+	}
+	case '2':
+	{
+		cout << "Enter Third Name: ";
+		string ThirdName;
+		cin.get();
+		getline(cin, ThirdName);
+		return FindByThirdName(a1,ThirdName);
+	}
+	case '3':
+	{
+		cout << "Enter Adress: ";
+		string Adress;
+		cin.get();
+		getline(cin, Adress);
+		return FindByAdress(a1, Adress);
+	}
+	case '4':
+	{
+		cout << "Enter Phone: ";
+		string Phone;
+		cin.get();
+		getline(cin, Phone);
+		return FindByPhone(a1, Phone);
+	}
+	case '5':
+	{
+		cout << "Enter Year: ";
+		int Year;
+		cin >> Year;
+		return FindByYear(a1, Year);
+	}
+	case '6':
+	{
+		cout << "Enter Inditification Number: ";
+		int InditificationNumber;
+		cin >> InditificationNumber;
+		return FindByInd(a1, InditificationNumber);
+	}
+	case '7':
+	{
+		cout << "Enter Seniority: ";
+		int seniority;
+		cin >> seniority;
+		return a1.seniority == seniority;
+	}
+	case '8':
+	{
+		cout << "Enter Engine Capacity: ";
+		int EngineCapacity;
+		cin >> EngineCapacity;
+		return a1.EngineCapacity == EngineCapacity;
+	}
+	case '9':
+	{
+		cout << "Enter Body Volume: ";
+		int BodyVolume;
+		cin >> BodyVolume;
+		return a1.BodyVolume == BodyVolume;
+	}
+	case '10':
+	{
+		cout << "Enter Prize: ";
+		int prize;
+		cin >> prize;
+		return a1.prize == prize;
+	}
+	default:
+	{
+		cout << "There is not this find parametr!" << endl;
+		return false;
+	}
+
+	}
+}
+*/
+template<typename T>
+bool FindByName(const T a1, const string a2)
+{
+	return a1.Name == a2;
+}
+template<typename T>
+bool FindBySecondName(const T a1, const string a2)
+{
+	return a1.SecondName == a2;
+}
+template<typename T>
+bool FindByThirdName(const T a1, const string a2)
+{
+	return a1.ThirdName == a2;
+}
+template<typename T>
+bool FindByAdress(const T a1, const string a2)
+{
+	return a1.adress == a2;
+}
+template<typename T>
+bool FindByPhone(const T a1, const string a2)
+{
+	return a1.phone == a2;
+}
+template<typename T>
+bool FindByYear(const T a1, const int a2)
+{
+	return a1.year == a2;
+}
+//Индивидуальный поиск
+//поиск покупателя по идентификационному номеру
+bool FindByInd(const Buyer buyer, const string a)
+{
+	return buyer.InditificationNumber == a;
+} 
+ //поиск менеджера по трудовому стажу
+template<typename T>
+bool FindBySeniority(const T manager, const double seniority)
+{
+	return manager.seniority == seniority;
+}
+
+//Поиск автомобиля по обьему двигателя
+bool FindByEC(const Car car, double EC)
+{
+	return car.EngineCapacity == EC;
+}
+//поиск автомобиля по обьему кузова
+bool FindByBV(const Car car, double BV)
+{
+	return car.BodyVolume == BV;
+}
+//поиск машыны по цене
+bool FindByPrize(const Car car, double prize)
+{
+	return car.prize == prize;
+}
+//поиск автомобиля по модели
+bool FindByModel(const Car car, const string model)
+{
+	return car.Model == model;
+}
+//Удаляем проданную машину
+void DeleteCar(Car** cars,const Car& car, int CarsAmount)
+{
+	bool found = false;
+	for (int i(0); i < CarsAmount; i++)
+	{
+		if (!found && cars[i]->Name == car.Name && cars[i]->Model == car.Model && cars[i]->year == car.year)
+		{
+			cars[i]->CarDelete();
+			found = true;
+		}
+		if (CarsAmount - i != 1)
+		{
+			cars[i] = cars[i + 1];
+		}
+	}
+
+}
---
+++ Автомагазин.cpp
@@ -0,0 +1,635 @@
+﻿// Автомагазин.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include "Header.h"
+using namespace std;
+
+void Menu()
+{
+	cout << " 1 - Add cars \n";
+	cout << " 2 - Buy Car \n";
+	cout << " 3 - Output all avialeble cars \n";
+	cout << " 4 - Output selled cars \n";
+	cout << " 5 - Find Menu \n";
+	cout << endl;
+}
+void FindMenu()
+{
+	cout << "1 - Find Car \n";
+	cout << "2 - Find Manager \n";
+	cout << endl;
+}
+void FindCarMenu()
+{
+	cout << "1 - Find by Name \n";
+	cout << "2 - Find by Model \n";
+	cout << "3 - Find by Engine Capacity \n";
+	cout << "4 - Find by Body Volume \n";
+	cout << "5 - Find by Prize \n";
+	cout << endl;
+}
+void FindManagerMenu()
+{
+	cout << "1 - Find by Name \n";
+	cout << "2 - Find by Second Name \n";
+	cout << "3 - Find by Third Name \n";
+	cout << "4 - Find by Year \n";
+	cout << "5 - Find by Adress \n";
+	cout << "6 - Find by Phone \n";
+	cout << "7 - Find by Seniority \n";
+}
+
+void OutToFileCar(Car* cars, const int CarsAmount,ostream& os)
+{
+	for (int i(0); i < CarsAmount; i++)
+	{
+		os << cars[i].Name << " " << cars[i].Model << " " << cars[i].year << " " << cars[i].EngineCapacity << " " << cars[i].BodyVolume <<
+			" " << cars[i].prize << endl;
+	}
+}
+void InputFromFileCar(Car** cars, const int CarsAmount, istream& is)
+{
+	for (int i(0); i < CarsAmount; i++)
+	{
+		is >> cars[i]->Name >> cars[i]->Model >> cars[i]->year >> cars[i]->EngineCapacity >> cars[i]->BodyVolume >> cars[i]->prize;
+	}
+}
+
+void OutToFileManager(Manager* managers, const int ManagerAmount, ostream& os)
+{
+	for (int i(0); i < ManagerAmount; i++)
+	{
+		os << managers[i].Name << " " << managers[i].SecondName << " " << managers[i].ThirdName << " " << managers[i].adress << " " << managers[i].phone <<
+			" " << managers[i].year << " " << managers[i].seniority << endl;
+	}
+}
+void InputFromFileManager(Manager* managers, const int ManagerAmount, istream& is)
+{
+	for (int i(0); i < ManagerAmount; i++)
+	{
+		is >> managers[i].Name >> managers[i].SecondName >> managers[i].ThirdName >> managers[i].adress >> managers[i].phone >> managers[i].year >> managers[i].seniority;
+	}
+}
+
+int main()
+{
+	char answer;
+	Car* cars = 0;
+	Car* SelledCars = 0;
+	Manager* managers = 0;
+	Buyer* buyers = 0;
+	int CarsAmount = 0, ManagerAmount = 0, BuyerAmount = 0,SelledCarsAmount = 0;
+	
+	//Ввод с файлов
+	//в 1 строке файла хранится количество записей
+	ifstream InAC("AvialebleCar.txt", ios::in);
+	InAC >> CarsAmount;
+	cars = AddStruct(cars, CarsAmount);
+	InputFromFileCar(&cars, CarsAmount, InAC);
+	InAC.close();
+	ifstream InSC("SelledCars.txt", ios::in);
+	InSC >> SelledCarsAmount;
+	SelledCars = AddStruct(SelledCars, SelledCarsAmount);
+	InputFromFileCar(&cars, SelledCarsAmount, InSC);
+	InSC.close();
+
+	do
+	{
+		Menu();
+		cout << "Input point of the menu: ";
+		cin >> answer;
+
+		switch (answer)
+		{
+		case '1':
+		{
+			Manager manager;
+			char Answer2;
+			cars = AddStruct(cars, CarsAmount);
+			SetCar(cars, CarsAmount);
+			do
+			{
+				cout << "Add manager? y/n: ";
+				cin >> Answer2;
+				if (Answer2 == 'y')
+				{
+					cin >> manager;
+					cars[CarsAmount].SetManager(manager);
+
+					managers = AddStruct(managers, ManagerAmount);
+					managers[ManagerAmount] = manager;
+					ManagerAmount++;
+				}
+			} while (Answer2 != 'n');
+
+			CarsAmount++;
+			break;
+		}
+		case '2':
+		{
+			//Ищем машину, которую купить хотим
+			//Переводим ее в масив структур машин, которые проданы
+			//Удаляем эту машину с доступных(хз, буду ли делать)
+			//Вводим данные покупателя
+			//Вводим имя менеджера, который нам ее посоветовал
+			//Приваиваем этому менеджеру этого покупателя
+			char FindAnswer;
+			Car FoundCar;
+			do
+			{
+				cout << "Enter name of car: ";
+				string CarName;
+				cin.get();
+				getline(cin, CarName);
+				
+				try
+				{
+					
+					//Нужно будет попробовать сократить количество функций поиска, используя одну с доп параметром, в который будем передавать значение с enum
+					//так не вышло
+					FoundCar = *find_if(cars, cars + CarsAmount, bind2nd(ptr_fun(FindByName<Car>), CarName));
+
+				}
+				catch (...)
+				{
+					cout << "No car found!" << endl;
+				}
+
+				if (FoundCar.buyer.Name.length() > 1)
+					cout << "Car was bought." << endl;
+				else cout << "Car was found! =)";
+
+				cout << "Do you want try one more time? y/n: ";
+				cin >> FindAnswer;
+
+			} while (FindAnswer != 'n');
+			
+			if (FoundCar.buyer.Name.length() <= 1)
+			{
+				SelledCars = AddStruct(SelledCars, SelledCarsAmount);
+
+				//вводим данные покупателя
+				cout << "Enter information about buyer: ";
+				Buyer buyer;
+				cin >> buyer;
+				FoundCar.SetBuyer(buyer);
+
+				//добавляем купленную машину в раздел проданных
+				SelledCars[SelledCarsAmount] = FoundCar;
+				SelledCarsAmount++;
+
+				//Удаляем проданную машину и уменьшаем количество машин на 1
+				DeleteCar(&cars, FoundCar, CarsAmount);
+				CarsAmount--;
+			}
+			break;
+		}
+		case '3':
+		{
+			for (int i(0); i < CarsAmount; i++)
+			{
+				cout << cars[i] << endl;
+			}
+			break;
+		}
+		case '4':
+		{
+			for (int i(0); i < SelledCarsAmount; i++)
+			{
+				cout << SelledCars[i] << endl;
+			}
+			break;
+		}
+		case '5':
+		{
+			char FindAnswer;
+			do
+			{
+				FindMenu();
+
+
+				cout << "Input point of the menu :";
+				cin >> FindAnswer;
+
+				switch (FindAnswer)
+				{
+				case '1':
+				{
+					Car FindCarCheck;
+					char FindCar;
+					do
+					{
+						FindCarMenu();
+						cout << "Input point of the menu :";
+						cin >> FindCar;
+						switch (FindCar)
+						{
+						case '1':
+						{
+							cout << "Enter name of car: ";
+							string CarName;
+							cin.get();
+							getline(cin, CarName);
+							try
+							{
+								FindCarCheck =  *find_if(cars, cars + CarsAmount, bind2nd(ptr_fun(FindByName<Car>), CarName));
+
+								if(FindCarCheck.Name != "")
+									cout << FindCarCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '2':
+						{
+							cout << "Enter model of car: ";
+							string Model;
+							cin.get();
+							getline(cin, Model);
+							try
+							{
+								FindCarCheck =  *find_if(cars, cars + CarsAmount, bind2nd(ptr_fun(FindByModel), Model));
+								if (FindCarCheck.Name != "")
+									cout << FindCarCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '3':
+						{
+							cout << "Enter Engine Capacity of car: ";
+							int EC;
+							cin >> EC;
+							try
+							{
+								FindCarCheck =  *find_if(cars, cars + CarsAmount, bind2nd(ptr_fun(FindByEC), EC));
+								if (FindCarCheck.Name != "")
+									cout << FindCarCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '4':
+						{
+							cout << "Enter Body Value of car: ";
+							int BV;
+							cin >> BV;
+							try
+							{
+								FindCarCheck = *find_if(cars, cars + CarsAmount, bind2nd(ptr_fun(FindByBV), BV));
+								if (FindCarCheck.Name != "")
+									cout << FindCarCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '5':
+						{
+							cout << "Enter Prize of car: ";
+							int Prize;
+							cin >> Prize;
+							try
+							{
+								FindCarCheck  = *find_if(cars, cars + CarsAmount, bind2nd(ptr_fun(FindByPrize), Prize));
+								if (FindCarCheck.Name != "")
+									cout << FindCarCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						default:
+							break;
+						}
+					} while (FindCar != 'o');
+					
+					break;
+				}
+				case '2':
+				{
+					Manager managerCheck;
+					char FindManager;
+					do
+					{
+						FindManagerMenu();
+						cout << "Input point of the menu :";
+						cin >> FindManager;
+
+						switch (FindManager)
+						{
+						case '1':
+						{
+							cout << "Enter name of manager: ";
+							string Name;
+							cin.get();
+							getline(cin, Name);
+							try
+							{
+								managerCheck = *find_if(managers, managers + ManagerAmount, bind2nd(ptr_fun(FindByName<Manager>), Name));
+
+								if (managerCheck.Name != "")
+									cout << managerCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '2':
+						{
+							cout << "Enter second name of manager: ";
+							string SecondName;
+							cin.get();
+							getline(cin, SecondName);
+							try
+							{
+								managerCheck = *find_if(managers, managers + ManagerAmount, bind2nd(ptr_fun(FindBySecondName<Manager>), SecondName));
+
+								if (managerCheck.Name != "")
+									cout << managerCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '3':
+						{
+							cout << "Enter third name of manager: ";
+							string ThirdName;
+							cin.get();
+							getline(cin, ThirdName);
+							try
+							{
+								managerCheck = *find_if(managers, managers + ManagerAmount, bind2nd(ptr_fun(FindByThirdName<Manager>), ThirdName));
+
+								if (managerCheck.Name != "")
+									cout << managerCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '4':
+						{
+							cout << "Enter birthday of manager: ";
+							int year;
+							cin >> year;
+							try
+							{
+								managerCheck = *find_if(managers, managers + ManagerAmount, bind2nd(ptr_fun(FindByYear<Manager>), year));
+
+								if (managerCheck.Name != "")
+									cout << managerCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '5':
+						{
+							cout << "Enter adress of manager: ";
+							string  adress;
+							cin.get();
+							getline(cin, adress);
+							try
+							{
+								managerCheck = *find_if(managers, managers + ManagerAmount, bind2nd(ptr_fun(FindByAdress<Manager>), adress));
+
+								if (managerCheck.Name != "")
+									cout << managerCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '6':
+						{
+							cout << "Enter phone of manager: ";
+							string  phone;
+							cin.get();
+							getline(cin, phone);
+							try
+							{
+								managerCheck = *find_if(managers, managers + ManagerAmount, bind2nd(ptr_fun(FindByPhone<Manager>), phone));
+
+								if (managerCheck.Name != "")
+									cout << managerCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						case '7':
+						{
+							cout << "Enter seniority of manager: ";
+							int  seniority;
+							cin >> seniority;
+							try
+							{
+								managerCheck = *find_if(managers, managers + ManagerAmount, bind2nd(ptr_fun(FindBySeniority<Manager>), seniority));
+
+								if (managerCheck.Name != "")
+									cout << managerCheck << endl;
+								else
+									cout << "Not Found!" << endl;
+							}
+							catch (const exception &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (const bad_alloc &a)
+							{
+								cout << "Error: " << a.what() << endl;
+							}
+							catch (...)
+							{
+								cout << "Unknown error!" << endl;
+							}
+							break;
+						}
+						default:
+							break;
+						}
+
+
+					} while (FindManager != 'o');
+					
+				}
+
+				default:
+					break;
+				}
+			} while (FindAnswer != 'o');
+
+			break;
+		}
+		default:
+			break;
+		}
+	}while (answer != 'o');
+
+	//Вывод в файл ここで日本語で書けますよ。おどろいた。では、始めましょう。
+	ofstream outAC("AvialebleCar.txt", ios::out);
+	ofstream outSC("SelledCar.txt", ios::out);
+	outAC << CarsAmount << endl;
+	if(CarsAmount != 0)
+		OutToFileCar(cars, CarsAmount, outAC);
+	outSC << SelledCarsAmount << endl;
+	if(SelledCarsAmount != 0)
+		OutToFileCar(SelledCars, SelledCarsAmount, outSC);
+	outAC.close();
+	outSC.close();
+
+
+	//cout << find(cars, cars + CarsAmount, 1)->Name<<endl;
+	//Car c = *find_if(cars, cars + CarsAmount, bind2nd(ptr_fun(FindByEC), 3.0));
+	//Manager man = *find_if(managers, managers + ManagerAmount, bind2nd(ptr_fun(FindByName<Manager>), "Kek"));
+	//cout << find_if(cars, cars + CarsAmount, bind2nd(mem_fun_ref(&Car::FindEC), 1));
+
+		_getch();
+		return 0;
+	}
+
